b9af69d5c10db22c6f700efc0a34e0d0c411e390
Caitlin <caitlinmccauley41@gmail.com>
2018-02-05 20:04:24 -0500

    FinalProjectFiles

---
+++ classwork/FinalProject/js/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+
+
+    $('.scripts--grid1--p').hide();
+
+    $('.scripts--grid1').click(function () {
+        $('.scripts--grid1--p').toggle('slow');
+        $('.scripts--grid1--h3').toggle('fast');
+    })
+
+
+
+    $('.scripts--grid2--p').hide();
+
+    $('.scripts--grid2').click(function () {
+        $('.scripts--grid2--p').toggle('slow');
+        $('.scripts--grid2--h3').toggle('fast');
+
+        $('.scripts1').css('width:10px')
+
+    })
+
+
+    $('.scripts--grid3--p').hide();
+
+    $('.scripts--grid3').click(function () {
+        $('.scripts--grid3--p').toggle('slow');
+        $('.scripts--grid3--h3').toggle('fast');
+
+    })
+
+    $('.scripts--grid4--p').hide();
+
+    $('.scripts--grid4').click(function () {
+        $('.scripts--grid4--p').toggle('slow');
+        $('.scripts--grid4--h3').toggle('fast');
+
+    })
+
+
+    // Name
+    $('.inputs__button--Name').click(function (name) {
+        var name = $('.inputs__Name').val();
+        $('.output--name').html(" " + name);
+    })
+
+    // ZipCode 
+    $('.inputs__button--Zip').click(function (name) {
+        var zip = $('.inputs__Zip').val();
+        $('.output--zip').html(" " + zip);
+    })
+
+
+})
+
